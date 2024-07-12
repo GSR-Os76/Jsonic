c66e8817cda4ac733288bfe783d82d1844de4c83
hmJ4Zor-Q-3 <129763409+hmJ4Zor-Q-3@users.noreply.github.com>
2024-07-12 13:12:00 -0400

    Fix TestJsonElement issues.

--- JsonicTests/TestJsonElement.cs
+++ JsonicTests/TestJsonElement.cs
@@ -6,32 +6,28 @@ namespace GSR.Tests.Jsonic
     [TestClass]
     public class TestJsonElement
     {
-
         [TestMethod]
         public void TestNullValue() 
         {
             Assert.AreEqual(JsonUtil.JSON_NULL, new JsonElement((JsonObject?)null).ToString());
             Assert.AreEqual(JsonUtil.JSON_NULL, new JsonElement().ToString());
+            Assert.AreEqual(JsonUtil.JSON_NULL, new JsonElement((JsonObject?)null).ToCompressedString());
+            Assert.AreEqual(JsonUtil.JSON_NULL, new JsonElement().ToCompressedString());
         } // end TestNullValue()
 
         [TestMethod]
-        [DataRow(false, JsonOptions.Default, JsonUtil.JSON_FALSE)]
-        [DataRow(true, JsonOptions.Default, JsonUtil.JSON_TRUE)]
-        [DataRow(false, JsonOptions.Compress, JsonUtil.JSON_FALSE)]
-        [DataRow(true, JsonOptions.Compress, JsonUtil.JSON_TRUE)]
-        public void TestBooleanValue(bool b, JsonOptions o, string expectation)
+        [DataRow(false, JsonUtil.JSON_FALSE)]
+        [DataRow(true, JsonUtil.JSON_TRUE)]
+        public void TestBooleanValue(bool b, string expectation)
         {
             JsonElement j = new(b);
-            j.Options = o;
             Assert.AreEqual(expectation, j.ToString());
+            Assert.AreEqual(expectation, j.ToCompressedString());
         } // end TestNullValue()
 
-        [TestMethod]
-        public void TestBooleanValue()
-        {
-            Assert.AreEqual(JsonUtil.JSON_FALSE, new JsonElement(false).ToString());
-            Assert.AreEqual(JsonUtil.JSON_TRUE, new JsonElement(true).ToString());
-        } // end TestNullValue()
+
+
+
 #warning others
 
     } // end class
