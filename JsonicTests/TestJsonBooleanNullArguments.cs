40f80b2eed0f8addd90b36d1e6c5771e82809b33
GSR-Os76 <122415667+GSR-Os76@users.noreply.github.com>
2024-07-29 13:58:51 -0400

    Throw NullArgumentExceptions at such arguments in JsonBoolean.

---
+++ JsonicTests/TestJsonBooleanNullArguments.cs
@@ -0,0 +1,26 @@
+﻿using GSR.Jsonic;
+
+namespace GSR.Tests.Jsonic
+{
+    [TestClass]
+    public class TestJsonBooleanNullArguments
+    {
+#pragma warning disable CS8625
+#pragma warning disable CS8600
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestParseJson1()
+        {
+            JsonBoolean.ParseJson(null, out _);
+        } // end TestParseJson1()
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestParseJson2()
+        {
+            JsonBoolean.ParseJson(null);
+        } // end TestParseJson2()
+#pragma warning restore CS8625
+#pragma warning restore CS8600
+    } // end class
+} // end namespace
