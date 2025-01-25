64f4da889363d235edaf72bd1a352d92388148cb
GSR-Os76 <122415667+GSR-Os76@users.noreply.github.com>
2025-01-25 11:56:22 -0500

    Update JsonNull tests.

---
+++ JsonicTests/TestJsonNull.cs
@@ -0,0 +1,49 @@
+﻿using GSR.Jsonic;
+
+namespace GSR.Tests.Jsonic
+{
+    public static class TestJsonNull
+    {
+        [TestClass]
+        public class Valid
+        {
+            [TestMethod]
+            [DataRow("null")]
+            public void ParseJson(string json)
+                => Assert.AreEqual(JsonNull.NULL, JsonNull.ParseJson(json));
+        } // end inner class Valid()
+
+        [TestClass]
+        public class Invalid
+        {
+#pragma warning disable CS8625
+
+
+
+            [TestMethod]
+            [ExpectedException(typeof(MalformedJsonException))]
+            [DataRow("nul")]
+            [DataRow("Null")]
+            public void ParseJsonNull(string json)
+                => JsonNull.ParseJson(json);
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentNullException))]
+            public void ParseJson()
+                => JsonNull.ParseJson(null);
+
+            [TestMethod]
+            [ExpectedException(typeof(MalformedJsonException))]
+            [DataRow("nul")]
+            [DataRow("Null")]
+            public void ParseJsonRemainderized(string json)
+                => JsonNull.ParseJson(json, out _);
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentNullException))]
+            public void ParseJsonRemainderizedNull()
+                => JsonNull.ParseJson(null, out _);
+#pragma warning restore CS8625
+        } // end inner class Invalid
+    } // end class
+} // end namespace
